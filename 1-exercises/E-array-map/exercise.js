f7f6a73b529a2e23c175f33747446e42f959a310
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-09-13 22:25:46 +0100

    using .map method
    Using 5 different call back functions for .map operation.

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -2,3 +2,27 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+
+function multiplier(number){ //this is the call-back function
+    return number * 10;
+}
+
+let multipliedNum0 = numbers.map(multiplier);
+//using call back function for .map
+console.log(multipliedNum0);
+
+let multipliedNum = numbers.map(function multiplied (num) {return num * 100});
+//declaring call-back function within the .map operation
+console.log(multipliedNum);
+
+let multipliedNum1 = numbers.map(function(num){return num * 20});
+//declaring the call-back function as an anonymous function 
+console.log(multipliedNum1);
+
+let multipliedNum3 = numbers.map(num => {return num * 40});
+//using arrow function by removing the function keyword
+console.log(multipliedNum3);
+
+let multipliedNum2 = numbers.map(num => num * 30);
+//shortening the arrow function by removing the curly braces and skipping return keyword
+console.log(multipliedNum2);
