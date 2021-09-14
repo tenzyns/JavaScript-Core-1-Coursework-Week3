c0338683ae9423463175a0484c0ad4ed6e02421b
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-09-14 18:55:17 +0100

    Update exercise.js
    forEach method used

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,29 @@
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+
+// function multipleOfNum(num){
+//  if (num % 3) {
+//    return "Fizz"
+//   } else if (num % 5){
+//     return "Buzz"
+//   } else if (num % 3 && num % 5){
+//     return "FizzBuzz"
+//   } else{
+//     return num;
+//   };
+// }
+arr.forEach(num => {
+  if  (num % 3 === 0 && num % 5 === 0){
+  console.log("FizzBuzz");
+} else if (num % 5 === 0) {
+  console.log("Buzz");
+} else if(num % 3 === 0){ 
+  console.log("Fizz");
+} else {
+  console.log(num);
+}});
+
 /* EXPECTED OUTPUT */
 
 /*
